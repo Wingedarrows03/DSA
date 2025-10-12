be1438807e952bbcf8793a1183cd6ad50579f635
Ashit Mallick <147731337+Wingedarrows03@users.noreply.github.com>
2025-10-12 19:36:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (94.01%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-bool AlphaNum(char c)
-        {
-            return(c>='A'&& c<='Z')||
-            (c>='a'&& c<='z')||
-            (c>='0'&& c<='9');
+bool alphanum(char c){
+    return
+    (c >= 'A' && c <= 'Z')||
+    (c >= 'a' && c <= 'z')||
+    (c >= '0' && c <= '9');
 
-        }
+}
     bool isPalindrome(string s) {
-    
-        int n = s.size();
-        int l = 0;// left pointer
-        int r = n-1; // right pointer 
-        while(l<r){
-            while (l<r && !AlphaNum(s[l])){
-                l++;
-            }
+    int n = s.size();
+    int l = 0;
+    int r = n-1;
 
-            while(l<r && !AlphaNum(s[r])){
-                r--;
-            }
-
-            if(tolower(s[l]) != tolower(s[r])){
-                return false;
-            }
-            l++; r--;
-
+    while(l<r){
+        while(l<r && !alphanum(s[l])){
+            l++;
         }
-        return true;
+
+        while(l<r && !alphanum(s[r])){
+            r--;
+        }
+
+        if(tolower(s[l]) != tolower(s[r])){
+            return false;
+        }
+        l++; r--;
+            
+        
+    }
+      return true; 
     }
 };
