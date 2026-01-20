12d68737e65706c790c0525bd97eaba1463d6995
Ashit Mallick <147731337+Wingedarrows03@users.noreply.github.com>
2026-01-20 11:01:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (77.30%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -2,15 +2,30 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        int cnt0=0, cnt1=0, cnt2=0;
-        for (int i =0;i<n;i++){
-            if (nums[i]==0) cnt0++;
-            else if (nums[i]==1) cnt1++;
-            else cnt2++;
-        }
-        for(int i=0;i<cnt0;i++) nums[i]=0;
-        for(int i=cnt0;i<cnt0+cnt1;i++) nums[i]=1;  
-        for(int i=cnt0+cnt1;i<n;i++) nums[i]=2;
+        int left = 0; // for 0s
+        int right = n-1; // for 2s
+        int mid = 0; // scanner
+
+        while(mid <= right){
+            if (nums[mid]== 0){
+                swap(nums[left],nums[mid]);
+                mid ++;
+                left++;
+            }
+
+            else if(nums[mid]==1){
+                    mid++;
+                }
+
+            else{
+                    swap(nums[right],nums[mid]);
+                    right --;
+
+                }
+
+            
 
         }
+
+    }
 };
