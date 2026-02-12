c54e076348f7764d00e33947f51574118118e1ff
Ashit Mallick <147731337+Wingedarrows03@users.noreply.github.com>
2026-02-12 09:59:28 +0530

    Time: 0 ms (100.00%) | Memory: 113.9 MB (27.51%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -4,27 +4,23 @@ public:
         int n = nums.size();
         int left = 0;
         int right = 0;
-
         long long windowsum = 0;
 
-        // Build first window
-        while (right < k) {
+        while( right < k){
             windowsum += nums[right];
-            right++;
+            right ++;
         }
 
         long long maxsum = windowsum;
 
-        // Slide the window
-        while (right < n) {
+        while(right < n){
             windowsum += nums[right];
             windowsum -= nums[left];
-            right++;
-            left++;
-
-            maxsum = max(maxsum, windowsum);
+            right ++;
+            left ++;
+            maxsum = max(maxsum,windowsum);
         }
-
-        return (double)maxsum / k;
+        
+        return (double)maxsum/k;
     }
 };
