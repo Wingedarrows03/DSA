641f83fa175c2054b55d2d7bc203ac53d8512422
Ashit Mallick <147731337+Wingedarrows03@users.noreply.github.com>
2026-02-17 12:23:40 +0530

    Time: 3 ms (78.05%) | Memory: 11.5 MB (62.57%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -3,40 +3,39 @@ public:
     vector<int> findAnagrams(string s, string p) {
         int n = s.size(); 
         int m = p.size();
-        vector<int> result; //array fot the result 
+        vector<int> result; //push my result here 
 
-        if (m > n) return result; //edge case 
-        
-        //freq map of p 
-        vector<int> target(26,0); // target vector (p)
-        for (char c : p){
-            target[c - 'a']++; //ASCII value ( x - 97 ==)
+        if(m > n) return result; // edge case handelling 
+
+        //freq map of m 
+        vector<int> target(26,0); // target freq map
+        for(char c : p){
+            target[c - 'a']++; //ASCII 
         }
 
-        //freq map of s 
-        vector<int> window(26,0); // window vector (s)
-
+        //freq map of n 
+        vector<int> window(26,0); // active window freq map 
         int right = 0; //scouting pointer 
-        int left = 0; //anchor / reducing pointer 
+        int left = 0; //anchor pointer / reducing pointer 
 
         while(right < n){
-            window[s[right]-'a']++; //ASCII value 
+            window[s[right] - 'a']++;
 
-            //window slide exceeds m
-            if(right - left + 1 > m){
-                window[s[left] - 'a']--;
+            // window size exceeds m
+            if( right - left + 1 > m){
+                window[s[left] - 'a']--; 
                 left ++;
             }
 
-            //window slide meets the m
-            if ( right - left + 1 == m){
-                if(window == target){
+            //window size meets the m
+            if( right - left + 1 == m){
+                if(window == target ){
                     result.push_back(left);
                 }
-                
             }
             right ++;
         }
         return result;
+
     }
 };
