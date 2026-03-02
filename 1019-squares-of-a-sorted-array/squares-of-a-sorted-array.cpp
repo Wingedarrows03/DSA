34542d2209e21c770edcc4ea801a09b821e8cb67
Ashit Mallick <147731337+Wingedarrows03@users.noreply.github.com>
2026-03-02 10:33:22 +0530

    Time: 3 ms (51.37%) | Memory: 31.2 MB (22.28%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -2,22 +2,28 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
-        int left = 0;
-        int right = n-1;
-        vector<int> rev;
-        while ( left <= right){
-            if ( nums[left] * nums[left] > nums[right]* nums[right]){
-                rev.push_back(nums[left]*nums[left]);
-                left ++;
+        vector<int> rev; // creating a result vector
+        int left = 0; //left pointer 
+        int right = n - 1; //right pointer
+
+        while ( left <= right)
+        {
+            // if left sqr is more than righ
+            if ( nums[left]* nums[left] > nums[right]* nums[right]){
+                rev.push_back(nums[left]*nums[left]); // pushing left sqr
+                left ++;// moving the left pointer fwd
             }
 
-            else
-            {
-                rev.push_back(nums[right]*nums[right]);
-                right --;
-            }
+            // when right sqr is more than left 
+            else {
+            rev .push_back(nums[right] * nums[right]); //pushing right sqr
+            right --;  // moving right pointer backwards
+
+            } 
+
         }
-        reverse(rev.begin(),rev.end());
-        return rev;
+        reverse(rev.begin(),rev.end()); // reversing the rev array
+        return rev; // output of answer 
+        
     }
 };
