01b224b11b82bf8586550cf20420c1a3b1bc85cf
Ashit Mallick <147731337+Wingedarrows03@users.noreply.github.com>
2026-03-02 11:07:08 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (79.52%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        // we will put 2 pointer right next to each other as all the repeating numbers are next to each other 
         int n = nums.size();
-        int i = 0; // Slow pointer for placing unique elements
+        int i = 0; // first pointer 
+        int j = i+1; // second pointer right next to first pointer 
         
-        for (int j = 1; j < n; j++) { // Fast pointer to scan the array
-            if (nums[j] != nums[i]) {
-                i++;
-                nums[i] = nums[j];
+        while ( j < n){
+            if (nums[i] != nums[j]){
+                i++; // first pointer will move 
+                nums[i] = nums[j]; 
+    
             }
+             j++; // second pointer will move too
+            
         }
+        return i+1; // as i starts from 0  
         
-        return i + 1;
     }
 };
