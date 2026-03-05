877951a3795433e30d21d97e02ee323b14f048a7
Ashit Mallick <147731337+Wingedarrows03@users.noreply.github.com>
2026-03-05 09:41:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (77.98%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -2,32 +2,30 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        int left = 0; // pointers for 0s
-        int right = n - 1; // pointers for 1s
-        int mid = 0; // pointers for scouting 
+        int left = 0; // left pointer for 0s
+        int right = n-1; // right pointer for 2s
+        int mid = 0; // scanner pointer
 
-        while(mid <= right){
-            // for 0s
-            if ( nums[mid] == 0){
-                swap(nums[left],nums[mid]); // swap it with left
-                left ++; // left will increase
-                mid ++; // mid will increase as well 
+        while( mid <= right){
+            // for 0s 
+            if(nums[mid] == 0){
+                swap(nums[left],nums[mid]); //swap it with left
+                left ++; //left pointer will increase 
+                mid ++; //mid pointer will increase as well 
             }
 
             //for 2s
-            else if ( nums[mid] == 2){
-                swap(nums[right],nums[mid]); // swap the mid with right
+            else if (nums[mid] == 2){
+                swap(nums[right],nums[mid]); //swap it with right
                 right --;
-                
             }
 
             //for 1s
-            else{
+            else {
                 mid ++;
             }
 
 
         }
-        
     }
 };
