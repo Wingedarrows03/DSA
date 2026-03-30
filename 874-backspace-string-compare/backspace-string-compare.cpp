fa84d30023b52ec521a90fb5317094c95d77626e
Ashit Mallick <147731337+Wingedarrows03@users.noreply.github.com>
2026-03-30 09:53:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (86.32%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,57 +1,70 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
+        int i = s.size() - 1; // pointer for s 
+        int j = t.size() - 1; // pointer for t
 
-        int i = s.size() - 1;
-        int j = t.size() - 1;
-
-        int cnti = 0;
-        int cntj = 0;
+        //counter
+        int cnti = 0; // counter for i
+        int cntj = 0; //counter for j
 
         while(i >= 0 || j >= 0){
-
-            // process s
+            // processing s 
             while(i >= 0){
-                if(s[i] == '#'){
-                    cnti++;
-                    i--;
+                // i encounters #
+                if (s[i] == '#'){
+                    cnti ++; // counter will increase 
+                    i --; // i will skip it 
                 }
-                else if(cnti > 0){
-                    cnti--;
-                    i--;
+                // if i ecounters valid char 
+                else if (cnti > 0){
+                    cnti --; //reduced the counter
+                    i--; // skipping the next char after #
                 }
                 else{
                     break;
                 }
+
             }
 
-            // process t
+            //processing t
             while(j >= 0){
+                // j encounters #
                 if(t[j] == '#'){
-                    cntj++;
-                    j--;
+                    cntj ++; //counter j will increase 
+                    j --; //j will skip #
                 }
                 else if(cntj > 0){
-                    cntj--;
-                    j--;
+                    // j encounter valid char 
+                    cntj --; // counter value will decrease 
+                    j --; // skip the next valid char after #
                 }
                 else{
                     break;
                 }
+
+            }
+            // comparing the strings 
+            if( i>=0 && j>=0){
+                if(s[i] != t[j]){
+                    // the letter is not matching
+                    return false;
+                }
+
             }
 
-            // compare characters
-            if(i >= 0 && j >= 0){
-                if(s[i] != t[j]) return false;
-            }
-            else if(i >= 0 || j >= 0){
-                return false;
+            // if any one of the string is still left with char
+           else  if( i>=0 || j>=0){
+            return false;
             }
 
-            i--;
-            j--;
+            // if pointers bypass the checks
+            i --;
+            j --;
+
         }
-
         return true;
+
+        
     }
 };
