6b3e84d6119e11b2fa939ed0c92268ef661e34ed
Ashit Mallick <147731337+Wingedarrows03@users.noreply.github.com>
2026-03-31 09:56:20 +0530

    Time: 43 ms (84.97%) | Memory: 29.2 MB (45.54%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -2,36 +2,58 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> result;
+        vector<vector<int>> result; //hold the result 
+        sort(nums.begin(),nums.end()); //sorting to handel duplicates easily 
 
-        sort(nums.begin(), nums.end());
+        int i = 0; //anchor pointer
 
-        for (int i = 0; i < n - 2; i++) {
-
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum < 0) {
-                    left++;
-                }
-                else if (sum > 0) {
-                    right--;
-                }
-                else {
-                    result.push_back({nums[i], nums[left], nums[right]});
-                    left++;
-                    right--;
-
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-                }
+        while(i <= n-3) // leaving enough space for 2 other pointers (i <= n-3)
+        { 
+            if(i>0 && nums[i] == nums[i-1])// handeling duplicates (looking backwards)
+            {
+                i ++;
+                continue;
             }
+            int left = i+1; //left pointer
+            int right = n-1; //right pointer
+
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right]; //sum 
+                // sum is less than target
+                if ( sum < 0){
+                    left ++;
+                }
+
+                //sum i smore than target 
+                else if(sum > 0){
+                    right --;
+                }
+
+                //hit the target 
+                else{
+                    result.push_back({nums[i],nums[left],nums[right]});
+
+                //handeling duplicates for left & right pointers
+                //using internal breaks
+                while(left < right && nums[right] == nums[right-1]){
+                    right --;
+                    continue;
+                }
+
+                while(left < right && nums[left] == nums[left+1]){
+                    left ++;
+                    continue;
+                }
+                //pointers must move 
+                left++;
+                right --;
+
+               }
+
+            }
+            i++; // move the anchor pointer 
         }
-        return result;
+        return result; //final answer
+        
     }
 };
