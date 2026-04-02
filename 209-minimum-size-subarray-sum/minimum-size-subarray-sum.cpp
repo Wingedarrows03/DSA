daa1bfac7f6d8db314b569c51932714324e8073b
Ashit Mallick <147731337+Wingedarrows03@users.noreply.github.com>
2026-04-02 11:45:56 +0530

    Time: 0 ms (100.00%) | Memory: 32.1 MB (40.45%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-
         int n = nums.size();
-        int left = 0;
-        int right = 0;
-        long long sum = 0;
-        int minlen = INT_MAX;
 
-        while(right < n){
+        //edge case handeling - 1
+        if (target < 0) return 0;
+
+        int left = 0; // left pointer 
+        int right = 0; //right pointer 
+        long long sum = 0; // sum 
+        int minlen = INT_MAX; // minimum length 
+
+        for(right = 0; right <= n-1; right++){
             sum += nums[right];
 
+            // if sum >= target
             while(sum >= target){
-                minlen = min(minlen, right - left + 1);
-                sum -= nums[left];
-                left++;
+                minlen = min(minlen, right - left + 1); //finding the minimum
+                sum -= nums[left]; // removing the oldest number 
+                left ++;
             }
 
-            right++;
+            
         }
-
-        if(minlen == INT_MAX){
-            return 0;
-        }
-
+        if (minlen == INT_MAX) return 0; // edge case handeling - 2
         return minlen;
+
     }
 };
