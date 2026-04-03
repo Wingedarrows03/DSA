1f266ac5222834db95b3ea4384f694c226144699
Ashit Mallick <147731337+Wingedarrows03@users.noreply.github.com>
2026-04-03 11:43:10 +0530

    Time: 31 ms (87.12%) | Memory: 85 MB (34.46%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         int n = fruits.size();
-        int left = 0;
-        int right = 0;
-        int maxlen = 0;
+        int left = 0; // left pointer 
+        int right = 0; //right pointer 
+        int maxfruits = 0; // maximum 
 
-        unordered_map<int,int> freq;
+        unordered_map<int,int> freq; // map to keep track of fequency and type of trees
 
-        while (right < n) {
-            freq[fruits[right]]++;
+        for(right = 0; right <= n-1; right++){
+            freq[fruits[right]]++; // window is expanding
 
-            while (freq.size() > 2) {
-                freq[fruits[left]]--;
-                if (freq[fruits[left]] == 0) {
-                    freq.erase(fruits[left]);
+            // when map size > 2
+            while(freq.size() > 2){
+                freq[fruits[left]]--; // removing the oldest element first
+                if (freq[fruits[left]] == 0){
+                    freq.erase(fruits[left]); // removing empty fruit types
                 }
-                left++;
+                left ++; // left pointer moving
+
             }
+            maxfruits = max(maxfruits,right-left+1);
 
-            maxlen = max(maxlen, right - left + 1);
-            right++;
         }
+        return maxfruits;
 
-        return maxlen;
     }
 };
