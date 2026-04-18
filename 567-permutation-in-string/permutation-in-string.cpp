1fe6a7cbde8b9abb01081aed6996815371a61750
Ashit Mallick <147731337+Wingedarrows03@users.noreply.github.com>
2026-04-18 11:16:49 +0530

    Time: 4 ms (51.95%) | Memory: 10 MB (69.26%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,46 +1,41 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+        int n1 = s1.size();
+        int n2 = s2.size();
 
-        // variable decleration 
-        int n = s2.size();
-        int m = s1.size();
-        vector<int> result;
+        //edge case handeling
+        if( n1 > n2 ) return false;
 
-        //edge case 
-        if(m > n){
-            return false;
+        // freq map for s1 (master list)
+        vector<int> target(26,0); 
+        for(char c: s1){
+            target [c - 'a']++; // ASCII Value
         }
 
-        //freq map for m
-        vector<int> target(26,0); // target fre map 
-        for(char c : s1){
-        target[c - 'a']++; //ASCII 
-        }
+        //freq map for s2 (window list)
+        vector<int> window(26,0);
+        int left = 0; //left pointer
+        int right = 0; //right pointer 
 
-        //freq map for n
-        vector<int> window(26,0); //active window map
-        int right = 0; // scouting pointer 
-        int left = 0; // anchor / shrinking pointer 
-        while ( right < n){
-            window[s2[right] - 'a']++;
+        for(right = 0; right <= n2-1; right ++){
+            window[s2[right] - 'a']++; // right pointer expanding
 
             //window exceeds the target
-            if(right - left + 1 > m){
-                window[s2[left] - 'a']--;
-                left ++;
+            if(right - left + 1 > n1){
+                window[s2[left] - 'a']--; //removing the oldest element
+                left ++; // left pointer increment 
             }
 
-            //window meets the target
-            if(right - left + 1 == m){
-                if(window == target){
+            // window meets the target
+            if(right - left + 1 == n1){
+                if (window == target){
                     return true;
                 }
+
             }
-            right ++;
+            
         }
         return false;
-
-
     }
 };
